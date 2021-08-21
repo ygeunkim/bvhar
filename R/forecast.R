9122efab28cc9c3250f5ff368f0a40fe0fd09afd
Young-geun Kim <dudrmshub@gmail.com>
2021-08-22 00:11:15 +0900

    add confidence intervals element in predict var function

--- R/forecast.R
+++ R/forecast.R
@@ -4,10 +4,25 @@
 #' 
 #' @param object \code{varlse} object
 #' @param n.ahead step to forecast
+#' @param level Specify alpha of confidence interval level 100(1 - alpha) percentage. By default, .05.
 #' @param ... not used
 #' @details 
 #' n-step ahead forecasting using VAR(p) recursively.
 #' See pp35 of Lütkepohl (2007).
+#' 
+#' Confidence interval at h-period is
+#' \deqn{y_{k,t}(h) \pm z_(\alpha / 2) \sigma_k (h)}
+#' 
+#' Joint forecast region of \eqn{100(1-\alpha)}\% can be computed by
+#' \deqn{\left\{ (y_{k, 1}, y_{k, h}) \mid y_{k, n}(i) - z_{(\alpha / 2h)} \sigma_n(i) \le y_{n, i} \le y_{k, n}(i) + z_{(\alpha / 2h)} \sigma_k(i), i = 1, \ldots, h \right\}}
+#' See the pp41 of Lütkepohl (2007).
+#' 
+#' To compute covariance matrix, it needs VMA representation:
+#' \deqn{Y_{t}(h) = c + \sum_{i = h}^{\infty} W_{i} \epsilon_{t + h - i} = c + \sum_{i = 0}^{\infty} W_{h + i} \epsilon_{t - i}}
+#' 
+#' Then
+#' 
+#' \deqn{\Sigma_y(h) = MSE \left[ y_t(h) \right] = \sum_{i = 0}^{h - 1} W_i \Sigma_{\epsilon} W_i^T = \Sigma_y(h - 1) + W_{h - 1} \Sigma_{\epsilon} W_{h - 1}^T}
 #' 
 #' @return matrix
 #' 
@@ -15,12 +30,26 @@
 #' Lütkepohl, H. (2007). \emph{New Introduction to Multiple Time Series Analysis}. Springer Publishing. \url{https://doi.org/10.1007/978-3-540-27752-1}
 #' @order 1
 #' @export
-predict.varlse <- function(object, n.ahead, ...) {
+predict.varlse <- function(object, n.ahead, level = .05, ...) {
   pred_res <- forecast_var(object, n.ahead)
   colnames(pred_res) <- colnames(object$y0)
+  SE <- 
+    compute_covmse(object, n.ahead) %>% # concatenated matrix
+    split.data.frame(gl(n.ahead, object$m)) %>% # list of forecast MSE covariance matrix
+    sapply(diag) %>% 
+    sqrt() %>% 
+    t() # extract only diagonal element to compute CIs
+  colnames(SE) <- colnames(object$y0)
+  z_quant <- qnorm(level / 2, lower.tail = FALSE)
+  z_bonferroni <- qnorm(level / (2 * n.ahead), lower.tail = FALSE)
   res <- list(
     process = "varlse",
     forecast = pred_res,
+    se = SE,
+    lower = pred_res - z_quant * SE,
+    upper = pred_res + z_quant * SE,
+    lower_joint = pred_res - z_bonferroni * SE,
+    upper_joint = pred_res + z_bonferroni * SE,
     y = object$y
   )
   class(res) <- "predbvhar"
--- src/bvharmisc.h
+++ src/bvharmisc.h
@@ -1,5 +1,5 @@
-#ifndef TSDESIGN_H
-#define TSDESIGN_H
+#ifndef BVHARMISC_H
+#define BVHARMISC_H
 
 SEXP build_y0(Eigen::MatrixXd x, int p, int t);
 
@@ -14,5 +14,9 @@ SEXP build_xdummy(int p, double lambda, Eigen::VectorXd sigma, double eps);
 SEXP minnesota_prior (Eigen::MatrixXd x_dummy, Eigen::MatrixXd y_dummy);
 
 SEXP build_ydummy_bvhar(Eigen::VectorXd sigma, double lambda, Eigen::VectorXd daily, Eigen::VectorXd weekly, Eigen::VectorXd monthly);
+
+SEXP VARtoVMA(Rcpp::List object, int lag_max);
+
+SEXP compute_covmse(Rcpp::List object, int step);
 
 #endif
