7dd11135032370650fcd916d531e75651274486b
Young-geun Kim <dudrmshub@gmail.com>
2021-08-28 23:18:45 +0900

    remove oxfordman data

--- data-raw/oxfordman.R
+++
@@ -1,6 +0,0 @@
-## code to prepare `oxfordman` dataset goes here
-library(readr)
-# import---------------------------------------
-oxfordman <- read_csv("data-raw/oxfordmanrealizedvolatilityindices.csv")
-
-usethis::use_data(oxfordman, overwrite = TRUE)
