3f974ce888c986872b401339f4b0afd1203486c0
Young Geun Kim <dudrmshub@gmail.com>
2021-12-16 17:10:23 +0900

    add bvhar test

---
+++ tests/testthat/test-bvhar-minnesota.R
@@ -0,0 +1,49 @@
+# Components of var-type--------------
+test_that("Test for VAR-type minnesota", {
+  etf_ncol <- ncol(etf_vix)
+  var_spec <- set_bvhar(
+    sigma = apply(etf_vix, 2, sd),
+    lambda = .2,
+    delta = rep(.1, etf_ncol)
+  )
+  fit_test_bvhar_var <- bvhar_minnesota(etf_vix, var_spec)
+  
+  expect_s3_class(var_spec, "bvharspec")
+  expect_s3_class(fit_test_bvhar_var, "bvharmn")
+  
+  expect_equal(
+    nrow(fit_test_bvhar_var$coef), 
+    ncol(etf_vix) * 3 + 1
+  )
+  expect_equal(
+    ncol(fit_test_bvhar_var$coef),
+    ncol(etf_vix)
+  )
+  
+})
+# Components of vhar-type--------------
+test_that("Test for VHAR-type minnesota", {
+  etf_ncol <- ncol(etf_vix)
+  vhar_spec <- set_weight_bvhar(
+    sigma = apply(etf_vix, 2, sd),
+    lambda = .2,
+    daily = rep(.3, etf_ncol),
+    weekly = rep(.2, etf_ncol),
+    monthly = rep(.1, etf_ncol)
+  )
+  fit_test_bvhar_vhar <- bvhar_minnesota(etf_vix, vhar_spec)
+  
+  expect_s3_class(vhar_spec, "bvharspec")
+  expect_s3_class(fit_test_bvhar_vhar, "bvharmn")
+  
+  expect_equal(
+    nrow(fit_test_bvhar_vhar$coef), 
+    ncol(etf_vix) * 3 + 1
+  )
+  expect_equal(
+    ncol(fit_test_bvhar_vhar$coef),
+    ncol(etf_vix)
+  )
+  
+})
+#> Test passed 🌈
