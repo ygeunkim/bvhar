e9fe57e17dec7b35798e8bca640d635de5b3b7a6
Young Geun Kim <dudrmshub@gmail.com>
2024-01-23 17:43:22 +0900

    rename source files corresponding to public header

--- src/bvhardraw.cpp
+++ src/bvhardraw.cpp
@@ -1,31 +1,5 @@
 #include <bvhardraw.h>
 
-// // Multivariate Gamma Function
-// // 
-// // Compute multivariate gamma function numerically
-// // 
-// // @param x Double, non-negative argument
-// // @param p Integer, dimension
-// double mgammafn(double x, int p) {
-//   if (p < 1) {
-//     Rcpp::stop("'p' should be larger than or same as 1.");
-//   }
-//   if (x <= 0) {
-//     Rcpp::stop("'x' should be larger than 0.");
-//   }
-//   if (p == 1) {
-//     return gammafn(x);
-//   }
-//   if (2 * x < p) {
-//     Rcpp::stop("'x / 2' should be larger than 'p'.");
-//   }
-//   double res = pow(M_PI, p * (p - 1) / 4.0);
-//   for (int i = 0; i < p; i++) {
-//     res *= gammafn(x - i / 2.0); // x + (1 - j) / 2
-//   }
-//   return res;
-// }
-
 //' Log of Multivariate Gamma Function
 //' 
 //' Compute log of multivariate gamma function numerically
