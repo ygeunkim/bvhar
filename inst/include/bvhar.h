936051bf1f6e81ba73320b6c18bf34d114e500d0
Young Geun Kim <ygeunkimstat@gmail.com>
2024-01-31 15:33:55 +0900

    add bvhar header

---
+++ inst/include/bvhar.h
@@ -0,0 +1,11 @@
+#ifndef BVHAR_H
+#define BVHAR_H
+
+#include "ols.h"
+#include "minnesota.h"
+#include "mcmcsv.h"
+#include "mcmcssvs.h"
+#include "mcmchs.h"
+#include "bvharinterrupt.h"
+
+#endif // BVHAR_H
