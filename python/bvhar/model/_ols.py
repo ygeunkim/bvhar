b1add6330a71de6ff84a012053dcbf656f58b6d6
Young Geun Kim <ygeunkimstat@gmail.com>
2024-08-31 10:32:49 +0900

    add other methods in ols that will be defined later

--- python/bvhar/model/_ols.py
+++ python/bvhar/model/_ols.py
@@ -11,23 +11,37 @@ class VarOls:
             "qr": 3
         }.get(method, None)
         self.y = check_np(data)
+        self.n_features_in_ = self.y.shape[1]
+        if self.y.shape[0] <= lag:
+            raise ValueError(f"'data' rows must be larger than `lag` = {lag}")
         self.p = lag
         self.fit_intercept = fit_intercept
         self._model = OlsVar(self.y, self.p, self.fit_intercept, self.method)
         # self.fit = {}
         self.coef_ = None
         self.intercept_ = None
-        self.n_features_in_ = self.y.ndim
+        self.cov_ = None
     
     def fit(self):
         fit = self._model.returnOlsRes()
         self.coef_ = fit.get("coefficients")
         self.intercept_ = get_var_intercept(self.coef_, self.p, self.fit_intercept)
+        self.cov_ = fit.get("covmat")
+    
+    def predict(self):
+        pass
 
-    # forecast method: predict()
-    # rolling window and expanding window
-    # spillover method: spillover()
-    # dynamic spillover
+    def roll_forecast(self):
+        pass
+
+    def expand_forecast(self):
+        pass
+
+    def spillover(self):
+        pass
+
+    def dynamic_spillover(self):
+        pass
 
 class VharOls:
     def __init__(self, data, week = 5, month = 22, fit_intercept = True, method = "nor"):
@@ -39,20 +53,34 @@ class VharOls:
             "qr": 3
         }.get(method, None)
         self.y = check_np(data)
+        self.n_features_in_ = self.y.shape[1]
+        if self.y.shape[0] <= month:
+            raise ValueError(f"'data' rows must be larger than `month` = {month}")
         self.week = week
         self.month = month
         self.fit_intercept = fit_intercept
         self._model = OlsVhar(self.y, self.week, self.month, self.fit_intercept, self.method)
         self.coef_ = None
         self.intercept_ = None
-        self.n_features_in_ = self.y.ndim
+        self.cov_ = None
     
     def fit(self):
         fit = self._model.returnOlsRes()
         self.coef_ = fit.get("coefficients")
         self.intercept_ = get_var_intercept(self.coef_, 3, self.fit_intercept)
+        self.cov_ = fit.get("covmat")
     
-    # forecast method: predict()
-    # rolling window and expanding window
-    # spillover method: spillover()
-    # dynamic spillover
+    def predict(self):
+        pass
+
+    def roll_forecast(self):
+        pass
+
+    def expand_forecast(self):
+        pass
+
+    def spillover(self):
+        pass
+
+    def dynamic_spillover(self):
+        pass
--- python/tests/test_ols.py
+++ python/tests/test_ols.py
@@ -1,5 +1,4 @@
-# import pytest
-# from bvhar.model import OlsVar, OlsVhar
+import pytest
 from bvhar.model import VarOls, VharOls
 import numpy as np
 
@@ -8,24 +7,21 @@ def test_var():
     dim_data = 3
     var_lag = 2
     data = np.random.randn(num_data, dim_data)
-    # fit_var = OlsVar(data, var_lag, True, 1)
-    # fit_var_llt = OlsVar(data, var_lag, True, 2)
-    # fit_var_qr = OlsVar(data, var_lag, True, 3)
-    # res = fit_var.return_ols_res()
-    # res_llt = fit_var_llt.return_ols_res()
-    # res_qr = fit_var_qr.return_ols_res()
-    # res_keys = ['coefficients', 'fitted.values', 'residuals', 'covmat', 'df', 'm', 'obs', 'y0',
-    #              'p', 'totobs', 'process', 'type', 'design', 'y']
-    # assert set(res_keys).issubset(set(res.keys()))
-    # assert set(res_keys).issubset(set(res_llt.keys()))
-    # assert set(res_keys).issubset(set(res_qr.keys()))
+
     fit_var = VarOls(data, var_lag, True, "nor")
     fit_var_llt = VarOls(data, var_lag, True, "chol")
     fit_var_qr = VarOls(data, var_lag, True, "qr")
     fit_var.fit()
     fit_var_llt.fit()
     fit_var_qr.fit()
-    
+
+    assert fit_var.n_features_in_ == dim_data
+    assert fit_var.coef_.shape == (dim_data * var_lag + 1, dim_data)
+    assert fit_var.intercept_.shape == (dim_data,)
+
+    data = np.random.randn(var_lag - 1, dim_data)
+    with pytest.raises(ValueError, match=f"'data' rows must be larger than `lag` = {var_lag}"):
+        fit_var = VarOls(data, var_lag, True, "nor")
 
 def test_vhar():
     num_data = 30
@@ -33,20 +29,18 @@ def test_vhar():
     week = 5
     month = 22
     data = np.random.randn(num_data, dim_data)
-    # fit_vhar = OlsVhar(data, week, month, True, 1)
-    # fit_vhar_llt = OlsVhar(data, week, month, True, 2)
-    # fit_vhar_qr = OlsVhar(data, week, month, True, 3)
-    # res = fit_vhar.return_ols_res()
-    # res_llt = fit_vhar_llt.return_ols_res()
-    # res_qr = fit_vhar_qr.return_ols_res()
-    # res_keys = ['coefficients', 'fitted.values', 'residuals', 'covmat', 'df', 'm', 'obs', 'y0',
-    #              'p', 'week', 'month', 'totobs', 'process', 'type', 'HARtrans', 'design', 'y']
-    # assert set(res_keys).issubset(set(res.keys()))
-    # assert set(res_keys).issubset(set(res_llt.keys()))
-    # assert set(res_keys).issubset(set(res_qr.keys()))
-    fit_vhar = VharOls(data, 5, 22, True, "nor")
-    fit_vhar_llt = VharOls(data, 5, 22, True, "chol")
-    fit_vhar_qr = VharOls(data, 5, 22, True, "qr")
+
+    fit_vhar = VharOls(data, week, month, True, "nor")
+    fit_vhar_llt = VharOls(data, week, month, True, "chol")
+    fit_vhar_qr = VharOls(data, week, month, True, "qr")
     fit_vhar.fit()
     fit_vhar_llt.fit()
     fit_vhar_qr.fit()
+
+    assert fit_vhar.n_features_in_ == dim_data
+    assert fit_vhar.coef_.shape == (dim_data * 3 + 1, dim_data)
+    assert fit_vhar.intercept_.shape == (dim_data,)
+
+    data = np.random.randn(month - 1, dim_data)
+    with pytest.raises(ValueError, match=f"'data' rows must be larger than `month` = {month}"):
+        fit_vhar = VharOls(data, week, month, True, "nor")
