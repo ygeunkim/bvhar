286622bbe1168e994ad3a6c0e5f61209e51e365b
Young Geun Kim <ygeunkimstat@gmail.com>
2024-10-27 17:48:59 +0900

    fix vhar stability check before forecast code [skip ci]

--- inst/include/ldltconfig.h
+++ inst/include/ldltconfig.h
@@ -341,9 +341,12 @@ struct LdltRecords : public RegRecords {
 		int dim = fac_record.cols();
 		int nrow_coef = num_alpha / dim;
 		std::vector<int> stable_id;
-		Eigen::MatrixXd var_record = coef_record * kronecker_eigen(Eigen::MatrixXd::Identity(dim, dim), har_trans);
+		// Eigen::MatrixXd var_record = coef_record.leftCols(num_alpha) * kronecker_eigen(Eigen::MatrixXd::Identity(dim, dim), har_trans);
 		for (int i = 0; i < coef_record.rows(); ++i) {
-			if (is_stable(var_record.row(i).head(num_alpha).reshaped(nrow_coef, dim))) {
+			// if (is_stable(var_record.row(i).reshaped(nrow_coef, dim))) {
+			// 	stable_id.push_back(i);
+			// }
+			if (is_stable(coef_record.row(i).head(num_alpha).reshaped(nrow_coef, dim), har_trans)) {
 				stable_id.push_back(i);
 			}
 		}
