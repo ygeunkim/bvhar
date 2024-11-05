45bc85f3d1780412ec5e59b76fa969eae3736ca3
Young Geun Kim <ygeunkimstat@gmail.com>
2024-11-05 21:24:45 +0900

    add forecastout method

--- inst/include/regforecaster.h
+++ inst/include/regforecaster.h
@@ -24,7 +24,7 @@ public:
 		last_pvec(Eigen::VectorXd::Zero(dim_design)),
 		sv_update(Eigen::VectorXd::Zero(dim)),
 		post_mean(Eigen::VectorXd::Zero(dim)),
-		// predictive_distn(Eigen::MatrixXd::Zero(step, num_sim * dim)),
+		predictive_distn(Eigen::MatrixXd::Zero(step, num_sim * dim)),
 		coef_mat(Eigen::MatrixXd::Zero(num_coef / dim, dim)),
 		contem_mat(Eigen::MatrixXd::Identity(dim, dim)),
 		standard_normal(Eigen::VectorXd::Zero(dim)),
@@ -36,61 +36,29 @@ public:
 		// tmp_vec = last_pvec.segment(dim, (var_lag - 1) * dim); // y_(T - 1), ... y_(T - lag + 1)
 	}
 	virtual ~RegForecaster() = default;
-	virtual void computeMean() = 0;
-	void updateParams(int i) {
-		coef_mat.topRows(nrow_coef) = unvectorize(reg_record->coef_record.row(i).head(num_alpha).transpose(), dim);
-		if (include_mean) {
-			coef_mat.bottomRows(1) = reg_record->coef_record.row(i).tail(dim);
-		}
-		sv_update = reg_record->fac_record.row(i).transpose().cwiseSqrt(); // D^1/2
-		contem_mat = build_inv_lower(dim, reg_record->contem_coef_record.row(i)); // L
-	}
-	void updateVariance() {
-		for (int j = 0; j < dim; ++j) {
-			standard_normal[j] = normal_rand(rng);
-		}
-		standard_normal.array() *= sv_update.array(); // D^(1/2) Z ~ N(0, D)
-	}
 	Eigen::MatrixXd forecastDensity() {
 		std::lock_guard<std::mutex> lock(mtx);
-		Eigen::MatrixXd predictive_distn(step, num_sim * dim); // rbind(step), cbind(sims)
+		// Eigen::MatrixXd predictive_distn(step, num_sim * dim); // rbind(step), cbind(sims)
 		Eigen::VectorXd obs_vec = last_pvec; // y_T, y_(T - 1), ... y_(T - lag + 1)
 		for (int i = 0; i < num_sim; ++i) {
 			last_pvec = obs_vec; // y_T, y_(T - 1), ... y_(T - lag + 1)
 			post_mean = obs_vec.head(dim); // y_T
 			tmp_vec = obs_vec.segment(dim, (var_lag - 1) * dim); // y_(T - 1), ... y_(T - lag + 1)
 			updateParams(i);
-			for (int h = 0; h < step; ++h) {
-				last_pvec.segment(dim, (var_lag - 1) * dim) = tmp_vec;
-				last_pvec.head(dim) = post_mean;
-				computeMean();
-				updateVariance();
-				post_mean += contem_mat.triangularView<Eigen::UnitLower>().solve(standard_normal); // N(post_mean, L^-1 D L^T-1)
-				predictive_distn.block(h, i * dim, 1, dim) = post_mean.transpose(); // hat(Y_{T + h}^{(i)})
-				tmp_vec = last_pvec.head((var_lag - 1) * dim);
-			}
+			forecastOut(i);
 		}
 		return predictive_distn;
 	}
 	Eigen::MatrixXd forecastDensity(const Eigen::VectorXd& valid_vec) {
 		std::lock_guard<std::mutex> lock(mtx);
-		Eigen::MatrixXd predictive_distn(step, num_sim * dim); // rbind(step), cbind(sims)
+		// Eigen::MatrixXd predictive_distn(step, num_sim * dim); // rbind(step), cbind(sims)
 		Eigen::VectorXd obs_vec = last_pvec; // y_T, y_(T - 1), ... y_(T - lag + 1)
 		for (int i = 0; i < num_sim; ++i) {
 			last_pvec = obs_vec;
 			post_mean = obs_vec.head(dim);
 			tmp_vec = obs_vec.segment(dim, (var_lag - 1) * dim);
 			updateParams(i);
-			for (int h = 0; h < step; ++h) {
-				last_pvec.segment(dim, (var_lag - 1) * dim) = tmp_vec;
-				last_pvec.head(dim) = post_mean;
-				computeMean();
-				updateVariance();
-				post_mean += contem_mat.triangularView<Eigen::UnitLower>().solve(standard_normal); // N(post_mean, L^-1 D L^T-1)
-				predictive_distn.block(h, i * dim, 1, dim) = post_mean.transpose(); // hat(Y_{T + h}^{(i)})
-				lpl[h] += sv_update.array().log().sum() - dim * log(2 * M_PI) / 2 - (sv_update.cwiseInverse().array() * (contem_mat * (post_mean - valid_vec)).array()).matrix().squaredNorm() / 2;
-				tmp_vec = last_pvec.head((var_lag - 1) * dim);
-			}
+			forecastOut(i, valid_vec);
 		}
 		lpl.array() /= num_sim;
 		return predictive_distn;
@@ -101,6 +69,7 @@ public:
 	double returnLpl() {
 		return lpl.mean();
 	}
+
 protected:
 	std::unique_ptr<LdltRecords> reg_record;
 	boost::random::mt19937 rng;
@@ -119,12 +88,53 @@ protected:
 	Eigen::VectorXd last_pvec; // [ y_(T + h - 1)^T, y_(T + h - 2)^T, ..., y_(T + h - p)^T, 1 ] (1 when constant term)
 	Eigen::VectorXd sv_update; // d_1, ..., d_m
 	Eigen::VectorXd post_mean; // posterior mean and y_(T + h - 1)
-	// Eigen::MatrixXd predictive_distn; // rbind(step), cbind(sims)
+	Eigen::MatrixXd predictive_distn; // rbind(step), cbind(sims)
 	Eigen::MatrixXd coef_mat; // include constant term when include_mean = true
 	Eigen::MatrixXd contem_mat; // L
 	Eigen::VectorXd standard_normal; // Z ~ N(0, I)
 	Eigen::VectorXd tmp_vec; // y_(T + h - 2), ... y_(T + h - lag)
 	Eigen::VectorXd lpl; // average log-predictive likelihood
+	virtual void computeMean() = 0;
+	void updateParams(int i) {
+		coef_mat.topRows(nrow_coef) = unvectorize(reg_record->coef_record.row(i).head(num_alpha).transpose(), dim);
+		if (include_mean) {
+			coef_mat.bottomRows(1) = reg_record->coef_record.row(i).tail(dim);
+		}
+		sv_update = reg_record->fac_record.row(i).transpose().cwiseSqrt(); // D^1/2
+		contem_mat = build_inv_lower(dim, reg_record->contem_coef_record.row(i)); // L
+	}
+	void updateVariance() {
+		for (int j = 0; j < dim; ++j) {
+			standard_normal[j] = normal_rand(rng);
+		}
+		standard_normal.array() *= sv_update.array(); // D^(1/2) Z ~ N(0, D)
+	}
+	void updateLpl(int h, const Eigen::VectorXd& valid_vec) {
+		lpl[h] += sv_update.array().log().sum() - dim * log(2 * M_PI) / 2 - (sv_update.cwiseInverse().array() * (contem_mat * (post_mean - valid_vec)).array()).matrix().squaredNorm() / 2;
+	}
+	void forecastOut(int i) {
+		for (int h = 0; h < step; ++h) {
+			last_pvec.segment(dim, (var_lag - 1) * dim) = tmp_vec;
+			last_pvec.head(dim) = post_mean;
+			computeMean();
+			updateVariance();
+			post_mean += contem_mat.triangularView<Eigen::UnitLower>().solve(standard_normal); // N(post_mean, L^-1 D L^T-1)
+			predictive_distn.block(h, i * dim, 1, dim) = post_mean.transpose(); // hat(Y_{T + h}^{(i)})
+			tmp_vec = last_pvec.head((var_lag - 1) * dim);
+		}
+	}
+	void forecastOut(int i, const Eigen::VectorXd& valid_vec) {
+		for (int h = 0; h < step; ++h) {
+			last_pvec.segment(dim, (var_lag - 1) * dim) = tmp_vec;
+			last_pvec.head(dim) = post_mean;
+			computeMean();
+			updateVariance();
+			post_mean += contem_mat.triangularView<Eigen::UnitLower>().solve(standard_normal); // N(post_mean, L^-1 D L^T-1)
+			predictive_distn.block(h, i * dim, 1, dim) = post_mean.transpose(); // hat(Y_{T + h}^{(i)})
+			updateLpl(h, valid_vec);
+			tmp_vec = last_pvec.head((var_lag - 1) * dim);
+		}
+	}
 };
 
 class RegVarForecaster : public RegForecaster {
@@ -140,6 +150,8 @@ public:
 		}
 	}
 	virtual ~RegVarForecaster() = default;
+
+protected:
 	void computeMean() override {
 		// post_mean = last_pvec.transpose() * coef_mat;
 		post_mean = coef_mat.transpose() * last_pvec;
@@ -159,13 +171,14 @@ public:
 		}
 	}
 	virtual ~RegVharForecaster() = default;
+	
+protected:
+	// Eigen::MatrixXd har_trans;
+	Eigen::SparseMatrix<double> har_trans;
 	void computeMean() override {
 		// post_mean = last_pvec.transpose() * har_trans.transpose() * coef_mat;
 		post_mean = coef_mat.transpose() * har_trans * last_pvec;
 	}
-protected:
-	// Eigen::MatrixXd har_trans;
-	Eigen::SparseMatrix<double> har_trans;
 };
 
 class RegVarSelectForecaster : public RegVarForecaster {
@@ -176,9 +189,12 @@ public:
 	RegVarSelectForecaster(const LdltRecords& records, const Eigen::MatrixXd& selection, int step, const Eigen::MatrixXd& response_mat, int lag, bool include_mean, bool filter_stable, unsigned int seed)
 	: RegVarForecaster(records, step, response_mat, lag, include_mean, filter_stable, seed), activity_graph(selection) {}
 	virtual ~RegVarSelectForecaster() = default;
+
+protected:
 	void computeMean() override {
 		post_mean = last_pvec.transpose() * (activity_graph.array() * coef_mat.array()).matrix();
 	}
+
 private:
 	Eigen::MatrixXd activity_graph; // Activity graph computed after MCMC
 };
@@ -191,9 +207,12 @@ public:
 	RegVharSelectForecaster(const LdltRecords& records, const Eigen::MatrixXd& selection, int step, const Eigen::MatrixXd& response_mat, const Eigen::MatrixXd& har_trans, int month, bool include_mean, bool filter_stable, unsigned int seed)
 	: RegVharForecaster(records, step, response_mat, har_trans, month, include_mean, filter_stable, seed), activity_graph(selection) {}
 	virtual ~RegVharSelectForecaster() = default;
+	
+protected:
 	void computeMean() override {
 		post_mean = last_pvec.transpose() * har_trans.transpose() * (activity_graph.array() * coef_mat.array()).matrix();
 	}
+
 private:
 	Eigen::MatrixXd activity_graph; // Activity graph computed after MCMC
 };
--- inst/include/svforecaster.h
+++ inst/include/svforecaster.h
@@ -25,7 +25,7 @@ public:
 		sv_update(Eigen::VectorXd::Zero(dim)),
 		sv_sig(Eigen::VectorXd::Zero(dim)),
 		post_mean(Eigen::VectorXd::Zero(dim)),
-		// predictive_distn(Eigen::MatrixXd::Zero(step, num_sim * dim)),
+		predictive_distn(Eigen::MatrixXd::Zero(step, num_sim * dim)),
 		coef_mat(Eigen::MatrixXd::Zero(num_coef / dim, dim)),
 		contem_mat(Eigen::MatrixXd::Identity(dim, dim)),
 		h_last_record(sv_record->lvol_record.rightCols(dim)),
@@ -38,6 +38,68 @@ public:
 		// tmp_vec = last_pvec.segment(dim, (var_lag - 1) * dim); // y_(T - 1), ... y_(T - lag + 1)
 	}
 	virtual ~SvForecaster() = default;
+	Eigen::MatrixXd forecastDensity() {
+		std::lock_guard<std::mutex> lock(mtx);
+		// Eigen::VectorXd mean_draw = post_mean.replicate(num_sim, 1); // cbind(sims)
+		// Eigen::MatrixXd predictive_distn(step, num_sim * dim); // rbind(step), cbind(sims)
+		Eigen::VectorXd obs_vec = last_pvec; // y_T y_(T - 1), ... y_(T - lag + 1)
+		for (int i = 0; i < num_sim; ++i) {
+			last_pvec = obs_vec;
+			post_mean = obs_vec.head(dim);
+			tmp_vec = obs_vec.segment(dim, (var_lag - 1) * dim);
+			updateParams(i);
+			forecastOut(i);
+		}
+		return predictive_distn;
+	}
+	Eigen::MatrixXd forecastDensity(const Eigen::VectorXd& valid_vec) {
+		std::lock_guard<std::mutex> lock(mtx);
+		// Eigen::MatrixXd predictive_distn(step, num_sim * dim); // rbind(step), cbind(sims)
+		Eigen::VectorXd obs_vec = last_pvec; // y_T y_(T - 1), ... y_(T - lag + 1)
+		for (int i = 0; i < num_sim; ++i) {
+			last_pvec = obs_vec;
+			post_mean = obs_vec.head(dim);
+			tmp_vec = obs_vec.segment(dim, (var_lag - 1) * dim);
+			updateParams(i);
+			forecastOut(i, valid_vec);
+		}
+		lpl.array() /= num_sim;
+		return predictive_distn;
+	}
+	Eigen::VectorXd returnLplRecord() {
+		return lpl;
+	}
+	double returnLpl() {
+		return lpl.mean();
+	}
+
+protected:
+	std::unique_ptr<SvRecords> sv_record;
+	boost::random::mt19937 rng;
+	std::mutex mtx;
+	Eigen::MatrixXd response; // y0
+	bool include_mean;
+	bool stable_filter;
+	bool sv;
+	int step;
+	int dim;
+	int var_lag; // VAR order or month order of VHAR
+	int dim_design;
+	int num_coef;
+	int num_alpha;
+	int nrow_coef; // dim_design in VAR and dim_har in VHAR (without constant term)
+	int num_sim;
+	Eigen::VectorXd last_pvec; // [ y_(T + h - 1)^T, y_(T + h - 2)^T, ..., y_(T + h - p)^T, 1 ] (1 when constant term)
+	Eigen::VectorXd sv_update; // h_(T + h)
+	Eigen::VectorXd sv_sig; // sig_h
+	Eigen::VectorXd post_mean; // posterior mean and y_(T + h - 1)
+	Eigen::MatrixXd predictive_distn; // rbind(step), cbind(sims)
+	Eigen::MatrixXd coef_mat; // include constant term when include_mean = true
+	Eigen::MatrixXd contem_mat; // L
+	Eigen::MatrixXd h_last_record; // h_T record
+	Eigen::VectorXd standard_normal; // Z ~ N(0, I)
+	Eigen::VectorXd tmp_vec; // y_(T + h - 2), ... y_(T + h - lag)
+	Eigen::VectorXd lpl; // average log-predictive likelihood
 	virtual void computeMean() = 0;
 	void updateParams(int i) {
 		coef_mat.topRows(nrow_coef) = unvectorize(sv_record->coef_record.row(i).head(num_alpha).transpose(), dim);
@@ -61,84 +123,32 @@ public:
 		}
 		standard_normal.array() *= (sv_update / 2).array().exp(); // D^(1/2) Z ~ N(0, D)
 	}
-	Eigen::MatrixXd forecastDensity() {
-		std::lock_guard<std::mutex> lock(mtx);
-		// Eigen::VectorXd mean_draw = post_mean.replicate(num_sim, 1); // cbind(sims)
-		Eigen::MatrixXd predictive_distn(step, num_sim * dim); // rbind(step), cbind(sims)
-		Eigen::VectorXd obs_vec = last_pvec; // y_T y_(T - 1), ... y_(T - lag + 1)
-		for (int i = 0; i < num_sim; ++i) {
-			last_pvec = obs_vec;
-			post_mean = obs_vec.head(dim);
-			tmp_vec = obs_vec.segment(dim, (var_lag - 1) * dim);
-			updateParams(i);
-			for (int h = 0; h < step; ++h) {
-				last_pvec.segment(dim, (var_lag - 1) * dim) = tmp_vec;
-				last_pvec.head(dim) = post_mean;
-				computeMean();
-				updateVariance();
-				post_mean += contem_mat.triangularView<Eigen::UnitLower>().solve(standard_normal); // N(post_mean, L^-1 D L^T-1)
-				predictive_distn.block(h, i * dim, 1, dim) = post_mean.transpose(); // hat(Y_{T + h}^{(i)})
-				tmp_vec = last_pvec.head((var_lag - 1) * dim);
-			}
+	void updateLpl(int h, const Eigen::VectorXd& valid_vec) {
+		lpl[h] += sv_update.sum() / 2 - dim * log(2 * M_PI) / 2 - ((-sv_update / 2).array().exp() * (contem_mat * (post_mean - valid_vec)).array()).matrix().squaredNorm() / 2;
+	}
+	void forecastOut(int i) {
+		for (int h = 0; h < step; ++h) {
+			last_pvec.segment(dim, (var_lag - 1) * dim) = tmp_vec;
+			last_pvec.head(dim) = post_mean;
+			computeMean();
+			updateVariance();
+			post_mean += contem_mat.triangularView<Eigen::UnitLower>().solve(standard_normal); // N(post_mean, L^-1 D L^T-1)
+			predictive_distn.block(h, i * dim, 1, dim) = post_mean.transpose(); // hat(Y_{T + h}^{(i)})
+			tmp_vec = last_pvec.head((var_lag - 1) * dim);
 		}
-		return predictive_distn;
 	}
-	Eigen::MatrixXd forecastDensity(const Eigen::VectorXd& valid_vec) {
-		std::lock_guard<std::mutex> lock(mtx);
-		Eigen::MatrixXd predictive_distn(step, num_sim * dim); // rbind(step), cbind(sims)
-		Eigen::VectorXd obs_vec = last_pvec; // y_T y_(T - 1), ... y_(T - lag + 1)
-		for (int i = 0; i < num_sim; ++i) {
-			last_pvec = obs_vec;
-			post_mean = obs_vec.head(dim);
-			tmp_vec = obs_vec.segment(dim, (var_lag - 1) * dim);
-			updateParams(i);
-			for (int h = 0; h < step; ++h) {
-				last_pvec.segment(dim, (var_lag - 1) * dim) = tmp_vec;
-				last_pvec.head(dim) = post_mean;
-				computeMean();
-				updateVariance();
-				post_mean += contem_mat.triangularView<Eigen::UnitLower>().solve(standard_normal); // N(post_mean, L^-1 D L^T-1)
-				predictive_distn.block(h, i * dim, 1, dim) = post_mean.transpose(); // hat(Y_{T + h}^{(i)})
-				lpl[h] += sv_update.sum() / 2 - dim * log(2 * M_PI) / 2 - ((-sv_update / 2).array().exp() * (contem_mat * (post_mean - valid_vec)).array()).matrix().squaredNorm() / 2;
-				tmp_vec = last_pvec.head((var_lag - 1) * dim);
-			}
+	void forecastOut(int i, const Eigen::VectorXd& valid_vec) {
+		for (int h = 0; h < step; ++h) {
+			last_pvec.segment(dim, (var_lag - 1) * dim) = tmp_vec;
+			last_pvec.head(dim) = post_mean;
+			computeMean();
+			updateVariance();
+			post_mean += contem_mat.triangularView<Eigen::UnitLower>().solve(standard_normal); // N(post_mean, L^-1 D L^T-1)
+			predictive_distn.block(h, i * dim, 1, dim) = post_mean.transpose(); // hat(Y_{T + h}^{(i)})
+			updateLpl(h, valid_vec);
+			tmp_vec = last_pvec.head((var_lag - 1) * dim);
 		}
-		lpl.array() /= num_sim;
-		return predictive_distn;
 	}
-	Eigen::VectorXd returnLplRecord() {
-		return lpl;
-	}
-	double returnLpl() {
-		return lpl.mean();
-	}
-protected:
-	std::unique_ptr<SvRecords> sv_record;
-	boost::random::mt19937 rng;
-	std::mutex mtx;
-	Eigen::MatrixXd response; // y0
-	bool include_mean;
-	bool stable_filter;
-	bool sv;
-	int step;
-	int dim;
-	int var_lag; // VAR order or month order of VHAR
-	int dim_design;
-	int num_coef;
-	int num_alpha;
-	int nrow_coef; // dim_design in VAR and dim_har in VHAR (without constant term)
-	int num_sim;
-	Eigen::VectorXd last_pvec; // [ y_(T + h - 1)^T, y_(T + h - 2)^T, ..., y_(T + h - p)^T, 1 ] (1 when constant term)
-	Eigen::VectorXd sv_update; // h_(T + h)
-	Eigen::VectorXd sv_sig; // sig_h
-	Eigen::VectorXd post_mean; // posterior mean and y_(T + h - 1)
-	// Eigen::MatrixXd predictive_distn; // rbind(step), cbind(sims)
-	Eigen::MatrixXd coef_mat; // include constant term when include_mean = true
-	Eigen::MatrixXd contem_mat; // L
-	Eigen::MatrixXd h_last_record; // h_T record
-	Eigen::VectorXd standard_normal; // Z ~ N(0, I)
-	Eigen::VectorXd tmp_vec; // y_(T + h - 2), ... y_(T + h - lag)
-	Eigen::VectorXd lpl; // average log-predictive likelihood
 };
 
 class SvVarForecaster : public SvForecaster {
@@ -154,6 +164,8 @@ public:
 		}
 	}
 	virtual ~SvVarForecaster() = default;
+
+protected:
 	void computeMean() override {
 		post_mean = last_pvec.transpose() * coef_mat;
 	}
@@ -162,9 +174,9 @@ public:
 class SvVharForecaster : public SvForecaster {
 public:
 	SvVharForecaster(const SvRecords& records, int step, const Eigen::MatrixXd& response_mat, const Eigen::MatrixXd& har_trans, int month, bool include_mean, bool filter_stable, bool sv, unsigned int seed)
-	: SvForecaster(records, step, response_mat, month, include_mean, filter_stable, sv, seed), har_trans(har_trans) {
+	: SvForecaster(records, step, response_mat, month, include_mean, filter_stable, sv, seed), har_trans(har_trans.sparseView()) {
 		if (stable_filter) {
-			sv_record->subsetStable(num_alpha, 1.05, har_trans.topLeftCorner(3 * dim, month * dim));
+			sv_record->subsetStable(num_alpha, 1.05, har_trans.topLeftCorner(3 * dim, month * dim).sparseView());
 			num_sim = sv_record->coef_record.rows();
 			if (num_sim == 0) {
 				STOP("No stable MCMC draws");
@@ -172,11 +184,13 @@ public:
 		}
 	}
 	virtual ~SvVharForecaster() = default;
+
+protected:
+	// Eigen::MatrixXd har_trans;
+	Eigen::SparseMatrix<double> har_trans;
 	void computeMean() override {
 		post_mean = last_pvec.transpose() * har_trans.transpose() * coef_mat;
 	}
-protected:
-	Eigen::MatrixXd har_trans;
 };
 
 class SvVarSelectForecaster : public SvVarForecaster {
@@ -187,9 +201,12 @@ public:
 	SvVarSelectForecaster(const SvRecords& records, const Eigen::MatrixXd& selection, int step, const Eigen::MatrixXd& response_mat, int lag, bool include_mean, bool filter_stable, bool sv, unsigned int seed)
 	: SvVarForecaster(records, step, response_mat, lag, include_mean, filter_stable, sv, seed), activity_graph(selection) {}
 	virtual ~SvVarSelectForecaster() = default;
+
+protected:
 	void computeMean() override {
 		post_mean = last_pvec.transpose() * (activity_graph.array() * coef_mat.array()).matrix();
 	}
+
 private:
 	Eigen::MatrixXd activity_graph; // Activity graph computed after MCMC
 };
@@ -202,9 +219,12 @@ public:
 	SvVharSelectForecaster(const SvRecords& records, const Eigen::MatrixXd& selection, int step, const Eigen::MatrixXd& response_mat, const Eigen::MatrixXd& har_trans, int month, bool include_mean, bool filter_stable, bool sv, unsigned int seed)
 	: SvVharForecaster(records, step, response_mat, har_trans, month, include_mean, filter_stable, sv, seed), activity_graph(selection) {}
 	virtual ~SvVharSelectForecaster() = default;
+
+protected:
 	void computeMean() override {
 		post_mean = last_pvec.transpose() * har_trans.transpose() * (activity_graph.array() * coef_mat.array()).matrix();
 	}
+
 private:
 	Eigen::MatrixXd activity_graph; // Activity graph computed after MCMC
 };
