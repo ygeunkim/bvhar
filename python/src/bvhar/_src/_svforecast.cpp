d2e2e0a3f0b163bf39b9aa7dbdb3e24ed75a24f6
Young Geun Kim <ygeunkimstat@gmail.com>
2024-11-22 21:25:04 +0900

    update python code for ggl option

--- python/src/bvhar/_src/_svforecast.cpp
+++ python/src/bvhar/_src/_svforecast.cpp
@@ -45,4 +45,44 @@ PYBIND11_MODULE(_svforecast, m) {
 			const Eigen::MatrixXi&, const Eigen::VectorXi&, int, bool>()
 		)
 		.def("returnForecast", &bvhar::McmcVharforecastRun<bvhar::McmcExpandforecastRun, bvhar::SvForecaster>::returnForecast);
+	
+	py::class_<bvhar::McmcVarforecastRun<bvhar::McmcRollforecastRun, bvhar::SvForecaster, false>>(m, "SvGrpVarRoll")
+		.def(
+			py::init<const Eigen::MatrixXd&, int, int, int, int, int,
+			bool, double, py::dict&, py::dict&, py::dict&, py::dict&, std::vector<py::dict>&, int,
+			const Eigen::VectorXi&, const Eigen::VectorXi&, const Eigen::VectorXi&, const Eigen::MatrixXi&,
+			bool, bool, int, const Eigen::MatrixXd&, bool,
+			const Eigen::MatrixXi&, const Eigen::VectorXi&, int, bool>()
+		)
+		.def("returnForecast", &bvhar::McmcVarforecastRun<bvhar::McmcRollforecastRun, bvhar::SvForecaster, false>::returnForecast);
+	
+	py::class_<bvhar::McmcVarforecastRun<bvhar::McmcExpandforecastRun, bvhar::SvForecaster, false>>(m, "SvGrpVarExpand")
+		.def(
+			py::init<const Eigen::MatrixXd&, int, int, int, int, int,
+			bool, double, py::dict&, py::dict&, py::dict&, py::dict&, std::vector<py::dict>&, int,
+			const Eigen::VectorXi&, const Eigen::VectorXi&, const Eigen::VectorXi&, const Eigen::MatrixXi&,
+			bool, bool, int, const Eigen::MatrixXd&, bool,
+			const Eigen::MatrixXi&, const Eigen::VectorXi&, int, bool>()
+		)
+		.def("returnForecast", &bvhar::McmcVarforecastRun<bvhar::McmcExpandforecastRun, bvhar::SvForecaster, false>::returnForecast);
+	
+	py::class_<bvhar::McmcVharforecastRun<bvhar::McmcRollforecastRun, bvhar::SvForecaster, false>>(m, "SvGrpVharRoll")
+		.def(
+			py::init<const Eigen::MatrixXd&, int, int, int, int, int, int,
+			bool, double, py::dict&, py::dict&, py::dict&, py::dict&, std::vector<py::dict>&, int,
+			const Eigen::VectorXi&, const Eigen::VectorXi&, const Eigen::VectorXi&, const Eigen::MatrixXi&,
+			bool, bool, int, const Eigen::MatrixXd&, bool,
+			const Eigen::MatrixXi&, const Eigen::VectorXi&, int, bool>()
+		)
+		.def("returnForecast", &bvhar::McmcVharforecastRun<bvhar::McmcRollforecastRun, bvhar::SvForecaster, false>::returnForecast);
+	
+	py::class_<bvhar::McmcVharforecastRun<bvhar::McmcExpandforecastRun, bvhar::SvForecaster, false>>(m, "SvGrpVharExpand")
+		.def(
+			py::init<const Eigen::MatrixXd&, int, int, int, int, int, int,
+			bool, double, py::dict&, py::dict&, py::dict&, py::dict&, std::vector<py::dict>&, int,
+			const Eigen::VectorXi&, const Eigen::VectorXi&, const Eigen::VectorXi&, const Eigen::MatrixXi&,
+			bool, bool, int, const Eigen::MatrixXd&, bool,
+			const Eigen::MatrixXi&, const Eigen::VectorXi&, int, bool>()
+		)
+		.def("returnForecast", &bvhar::McmcVharforecastRun<bvhar::McmcExpandforecastRun, bvhar::SvForecaster, false>::returnForecast);
 }
