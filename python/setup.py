d6bdf001e0b716b9b1e52daefb8a6de0339e15ea
Young Geun Kim <ygeunkimstat.gmail.com>
2024-12-15 14:45:33 +0900

    try spdlog_compile_lib instead of external fmt

--- python/setup.py
+++ python/setup.py
@@ -113,17 +113,19 @@ def find_module(base_dir):
                         define_macros=[
                             ('EIGEN_PERMANENTLY_DISABLE_STUPID_WARNINGS', None),
                             ('BOOST_ENABLE_ASSERT_HANDLER', None),
-                            ('SPDLOG_FMT_EXTERNAL', None),
-                            ('FMT_HEADER_ONLY', None)
+                            # ('SPDLOG_FMT_EXTERNAL', None),
+                            # ('FMT_HEADER_ONLY', None)
+                            ('SPDLOG_COMPILE_LIB', None)
                         ],
                         include_dirs=[
                             include_path,
-                            str(HeaderInclude('fmt')),
+                            # str(HeaderInclude('fmt')),
                             str(HeaderInclude('spdlog')),
                             str(HeaderInclude('eigen3')),
                             str(HeaderInclude('boost'))
                         ],
-                        library_dirs = lib_path
+                        library_dirs = lib_path,
+                        libraries = ['fmt']
                     )
                 )
     return extensions
