c6a4fef32fdce9e87798c56a63bcef732bbfa9be
Young Geun Kim <ygeunkimstat@gmail.com>
2025-02-05 22:48:01 -0800

    remove sim_gig index in pkgdown

--- inst/include/bvhar/src/math/random.h
+++ inst/include/bvhar/src/math/random.h
@@ -4,16 +4,6 @@
 #include "../core/common.h"
 #include <vector>
 
-#ifdef USE_RCPP
-Eigen::MatrixXd sim_mgaussian(int num_sim, Eigen::VectorXd mu, Eigen::MatrixXd sig);
-
-Eigen::MatrixXd sim_mstudent(int num_sim, double df, Eigen::VectorXd mu, Eigen::MatrixXd sig, int method);
-
-Eigen::MatrixXd sim_matgaussian(Eigen::MatrixXd mat_mean, Eigen::MatrixXd mat_scale_u, Eigen::MatrixXd mat_scale_v);
-
-Eigen::MatrixXd sim_iw(Eigen::MatrixXd mat_scale, double shape);
-#endif
-
 namespace bvhar {
 
 inline Eigen::MatrixXd sim_mgaussian_chol(int num_sim, const Eigen::VectorXd& mu, const Eigen::MatrixXd& sig, boost::random::mt19937& rng) {
