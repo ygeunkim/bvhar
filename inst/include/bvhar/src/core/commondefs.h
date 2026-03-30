fe1928069ac7d623fe8ea4d3ecd143c04dfbe9ea
ygeunkim <ygeunkimstat@gmail.com>
2026-03-30 21:04:35 +0900

    add pure c++ macros for Rcpp::List

--- inst/include/bvhar/src/core/commondefs.h
+++ inst/include/bvhar/src/core/commondefs.h
@@ -106,19 +106,84 @@
 	#define BVHAR_IS_LOGICAL(element) py::detail::type_caster<Eigen::Matrix<bool, Eigen::Dynamic, 1>>().load(element, false)
 	#define BVHAR_CAST_VECTOR(element) py::cast<Eigen::VectorXd>(element)
 	#define BVHAR_CAST_MATRIX(element) py::cast<Eigen::MatrixXd>(element)
-
-	#ifndef M_PI
-		// Some platform does not have M_PI defined - to the same value as in Rmath.h
-		#define M_PI 3.141592653589793238462643383280
-	#endif
 #else
 	// Pure C++
+	#include <cmath>
+	#include <cstdio>
 	#include <map>
   #include <string>
   #include <any>
   #include <vector>
   #include <iostream>
   #include <stdexcept>
+
+	#define Rf_gammafn(x) std::tgamma(x)
+	#define Rf_lgammafn(x) std::lgamma(x)
+	#define Rf_dgamma(x, shp, scl, lg) (lg ? log((shp - 1) * log(x) - x / scl - std::lgamma(shp) - shp * log(scl)) : exp((shp - 1) * log(x) - x / scl - std::lgamma(shp) - shp * log(scl)))
+
+	using BvharList = std::map<std::string, std::any>;
+
+	struct BvharNamed {
+    std::string name;
+    explicit BvharNamed(const std::string& n) : name(n) {}
+    
+    template <typename T>
+    std::pair<const std::string, std::any> operator=(T&& val) {
+      return {name, std::forward<T>(val)};
+    }
+  };
+
+  template <typename... Args>
+  BvharList create_bvhar_list(Args&&... args) {
+    return BvharList{ std::forward<Args>(args)... };
+  }
+
+	#define BVHAR_LIST BvharList
+	#define BVHAR_LIST_OF_LIST std::vector<BvharList>
+	#define BVHAR_PY_LIST std::vector<std::any>
+	#define BVHAR_WRAP(value) std::any(value)
+	#define BVHAR_CAST std::any_cast
+  #define BVHAR_CAST_DOUBLE(value) std::any_cast<double>(value)
+  #define BVHAR_CAST_INT(value) std::any_cast<int>(value)
+  #define BVHAR_CAST_BOOL(value) std::any_cast<bool>(value)
+	#define BVHAR_CONTAINS(container, key) (container.find(key) != container.end())
+	#define BVHAR_CREATE_LIST(...) create_bvhar_list(__VA_ARGS__)
+  #define BVHAR_NAMED BvharNamed
+  #define BVHAR_ACCESS_LIST(iterator, list) iterator.second
+
+	inline void stop_fmt(const std::string& msg) {
+		throw std::runtime_error(msg);
+	}
+
+	// Use .c_str() instead of passing std::string
+	template <typename... Args>
+	inline void stop_fmt(const char* fmt, Args... args) {
+		int n = std::snprintf(nullptr, 0, fmt, std::forward<Args>(args)...);
+		if (n < 0) {
+			throw std::runtime_error("BVHAR_STOP formatting failed");
+		}
+		std::string buf(static_cast<size_t>(n), '\0');
+		std::snprintf(buf.data(), static_cast<size_t>(n) + 1, fmt, std::forward<Args>(args)...);
+		throw std::runtime_error(buf);
+	}
+
+  #define BVHAR_STOP(...) stop_fmt(__VA_ARGS__)
+  #define BVHAR_COUT std::cout
+  #define BVHAR_ENDL std::endl
+  #define BVHAR_FLUSH std::cout.flush()
+  #define BVHAR_STRING std::string
+
+	#define BVHAR_IS_MATRIX(element) (element.type() == typeid(Eigen::MatrixXd) || element.type() == typeid(Eigen::MatrixXi) || element.type() == typeid(Eigen::Matrix<bool, Eigen::Dynamic, Eigen::Dynamic>))
+  #define BVHAR_IS_VECTOR(element) (element.type() == typeid(Eigen::VectorXd) || element.type() == typeid(Eigen::VectorXi) || element.type() == typeid(Eigen::Matrix<bool, Eigen::Dynamic, 1>))
+  #define BVHAR_IS_LOGICAL(element) (element.type() == typeid(bool))
+  
+  #define BVHAR_CAST_VECTOR(element) std::any_cast<Eigen::VectorXd>(element)
+  #define BVHAR_CAST_MATRIX(element) std::any_cast<Eigen::MatrixXd>(element)
+#endif
+
+#ifndef M_PI
+	// Some platform does not have M_PI defined - to the same value as in Rmath.h
+	#define M_PI 3.141592653589793238462643383280
 #endif
 
 #include <memory>
